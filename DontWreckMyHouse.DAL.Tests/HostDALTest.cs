c043eeb6854bf20426e0c03dc43c3f7079431d61
Aaron Stolle <aaronstolle@gmail.com>
2022-04-07 10:24:06 -0500

    Add HostDALMethod and HostDALTest

--- DontWreckMyHouse.DAL.Tests/HostDALTest.cs
+++ DontWreckMyHouse.DAL.Tests/HostDALTest.cs
@@ -1,18 +1,58 @@
-﻿using NUnit.Framework;
+﻿using DontWreckMyHouse.Core.Models;
+using NUnit.Framework;
+using System.IO;
 
 namespace DontWreckMyHouse.DAL.Tests
 {
     public class HostTests
     {
+        const string SEED_DIRECTORY = "TestData";
+        const string SEED_FILE = "DALHostTest.csv";
+        const string TEST_DIRECTORY = "test";
+        const string TEST_FILE = "host.dat";
+
+        string SEED_PATH = Path.Combine(SEED_DIRECTORY, SEED_FILE);
+        string TEST_PATH = Path.Combine(TEST_DIRECTORY, TEST_FILE);
+
+        HostRepository repo;
+
         [SetUp]
         public void Setup()
         {
+            if (!Directory.Exists(TEST_DIRECTORY))
+            {
+                Directory.CreateDirectory(TEST_DIRECTORY);
+            }
+
+            File.Copy(SEED_PATH, TEST_PATH, true);
+            repo = new HostRepository(TEST_PATH, new HostFormatter());
         }
 
         [Test]
-        public void Test1()
+        public void TestFindByEmail()
         {
-            Assert.Pass();
+            //Arrange
+            Host expected = new Host()
+            {
+                Id = "3edda6bc-ab95-49a8-8962-d50b53f84b15",
+                LastName = "Yearnes",
+                Email = "eyearnes0@sfgate.com",
+                Phone = "(806) 1783815",
+                Address = "3 Nova Trail",
+                City = "Amarillo",
+                State = "TX",
+                PostalCode = 79182,
+                StandardRate = 340M,
+                WeekendRate = 425M
+            };
+
+            //Act
+            Result<Host> actual = repo.FindByEmail("eyearnes0@sfgate.com");
+
+            //Assert
+            Assert.IsTrue(actual.Data.ToString() == expected.ToString());
+            Assert.IsTrue(actual.Success);
+            Assert.AreEqual(expected.Email, actual.Data.Email);
         }
     }
 }
--- DontWreckMyHouse.DAL/HostRepository.cs
+++ DontWreckMyHouse.DAL/HostRepository.cs
@@ -8,9 +8,45 @@ namespace DontWreckMyHouse.DAL
 {
     public class HostRepository : IHost
     {
+        public string DataFilePath { get; set; }
+        public ICustomeFormatter<Host> Formatter { get; set; }
+
+        public HostRepository(string dataFilePath, ICustomeFormatter<Host> formatter)
+        {
+            DataFilePath = dataFilePath;
+            Formatter = formatter;
+        }
         public Result<Host> FindByEmail(string email)
         {
-            throw new NotImplementedException();
+            Result<Host> result = new();
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(DataFilePath))
+                {
+                    string data = sr.ReadLine();
+                    data = sr.ReadLine();
+                    while(data != null)
+                    {
+                        Host host = Formatter.Deserialize(data);
+                        if(host.Email == email)
+                        {
+                            result.Data = host;
+                            result.Success = true;
+                            return result;
+                        }
+                        data = sr.ReadLine();
+                    }
+                }
+            }catch (Exception ex)
+            {
+                result.Success = false;
+                result.Message = ex.Message;
+                return result;
+            }
+            result.Success = false;
+            result.Message = "I couldn't find the email";
+            return result;
         }
     }
 }
